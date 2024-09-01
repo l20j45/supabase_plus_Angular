28a329c449048044565f05f12e46cc1bcc039f75
Daniel Rojas <l20j45_@gmail.com>
2024-09-01 11:02:19 -0600

    Cree los servicios de supabase, modifique los servicios de auth, y tambien cambie los formularios

--- src/app/auth/features/auth-sign-up/auth-sign-up.component.ts
+++ src/app/auth/features/auth-sign-up/auth-sign-up.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-auth-sign-up',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl:'auth-sign-up.component.html',
   styleUrl: './auth-sign-up.component.scss'
 })
--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {SignUpWithPasswordCredentials, SupabaseClient} from "@supabase/supabase-js";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(public supabaseCliente: SupabaseClient) {
+  }
+
+  signUp(credentials: SignUpWithPasswordCredentials) {
+    return this.supabaseCliente.auth.signUp(credentials);
+  }
+
+  logIn(credentials: SignUpWithPasswordCredentials) {
+    return this.supabaseCliente.auth.signInWithPassword(credentials);
+  }
+
+  signOut() {
+    return this.supabaseCliente.auth.signOut();
+  }
+
+  session() {
+  }
 }
+
