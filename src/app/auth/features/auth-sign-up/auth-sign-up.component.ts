4f4a706b1ca44e2b6ba3346a0e6515b7fc1c4342
Daniel Rojas <l20j45_@gmail.com>
2024-09-02 09:17:47 -0600

    Aqui ya funciona el signup

--- src/app/auth/features/auth-sign-up/auth-sign-up.component.ts
+++ src/app/auth/features/auth-sign-up/auth-sign-up.component.ts
@@ -1,15 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {FormBuilder, FormControl, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgIf} from "@angular/common";
+import {AuthService} from "../../service/auth.service";
+import {FormSignup} from "../../../shared/models/models";
+
 
 @Component({
   selector: 'app-auth-sign-up',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink, ReactiveFormsModule, NgIf
   ],
-  templateUrl:'auth-sign-up.component.html',
+  templateUrl: 'auth-sign-up.component.html',
   styleUrl: './auth-sign-up.component.scss'
 })
-export class AuthSignUpComponent {
+export class AuthSignUpComponent implements OnInit {
+  form: any;
+
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) {
+  }
+
+  ngOnInit() {
+    this.form = this.formBuilder.group<FormSignup>({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required]),
+    });
+  }
+
+  async submit() {
+    if (this.form.invalid) {
+      console.log(this.form.invalid)
+      return
+    }
+    const authResponse = await this.authService.signUp(this.form.value);
+
+    console.log(authResponse);
+  }
+
+  get email() {
+    return this.form.get('email');
+  }
+
+  get password() {
+    return this.form.get('password');
+  }
+
 
 }
--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -1,24 +1,27 @@
 import {Injectable} from '@angular/core';
-import {SignUpWithPasswordCredentials, SupabaseClient} from "@supabase/supabase-js";
+import {createClient, SignUpWithPasswordCredentials, SupabaseClient} from "@supabase/supabase-js";
+import {SupabaseService} from "../../shared/data-access/supabase.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  superBaseClient: SupabaseClient;
 
-  constructor(public supabaseCliente: SupabaseClient) {
+  constructor(public supaBaseService: SupabaseService) {
+    this.superBaseClient = supaBaseService.supabaseClient;
   }
 
   signUp(credentials: SignUpWithPasswordCredentials) {
-    return this.supabaseCliente.auth.signUp(credentials);
+    return this.superBaseClient.auth.signUp(credentials);
   }
 
   logIn(credentials: SignUpWithPasswordCredentials) {
-    return this.supabaseCliente.auth.signInWithPassword(credentials);
+    return this.superBaseClient.auth.signInWithPassword(credentials);
   }
 
   signOut() {
-    return this.supabaseCliente.auth.signOut();
+    return this.superBaseClient.auth.signOut();
   }
 
   session() {
