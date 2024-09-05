42f2c215df1c1a322f1f6521ccc8fd97429a9caf
daniel rojas <l20j45@hotmail.com>
2024-09-05 12:07:37 -0600

    Delete src/environments directory

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  SUPABASE_URL: 'https://wnvbhsdfabmvmzzsvecu.supabase.co',
-  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndudmJoc2RmYWJtdm16enN2ZWN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUxNDQyNTIsImV4cCI6MjA0MDcyMDI1Mn0.sRO7rfywK7934_seiJm_YCCt7bnu5sOZZ__q0Ip1AAk'
-
-};
